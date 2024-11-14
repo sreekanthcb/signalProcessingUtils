75bb597014ac99695c4ad0ae96d5a99407d39dcd
sreekanthcb <sreekanthcb@yahoo.com>
2024-11-14 14:20:36 +0530

    finding value of pi from random numbers

--- pi_fromRand.m
+++ pi_fromRand.m
@@ -1,18 +1,17 @@
-%%=========================================================================================
+%%=================================================================================================
 % Synopsis     : Finds value of pi from random numbers
-%              : idea is ratio of area of circle to square is 1/4th of pi (think about it)
 % Last updated : 2021-09-04
-%%=========================================================================================
+%==================================================================================================
 clc;clear;close all;
 
-n_points = 1e8;
+n_points = 1e8; % large value of this gives more accurate value for pi
 
-x       = rand(n_points,1);
+x       = rand(n_points,1); % array of random numbers between 0 and 1
 y       = rand(n_points,1);
 
-z       = sqrt(x.^2+y.^2);
-cPoints = length(find(z<=1));
+z       = sqrt(x.^2+y.^2);% z = (x,y) forms points inside first quadrant of a cartesian plain
+cPoints = length(find(z<=1)); % finding how many of this points lies inside a circle of unit radius
 
-CalcPi  = 4*cPoints/n_points;
+CalcPi  = 4*cPoints/n_points; % ratio of area of circle to square is 1/4th of pi (think about it)
 
 fprintf("Calculated value of pi is %f\n",CalcPi);
